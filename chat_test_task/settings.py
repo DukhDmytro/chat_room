2f154ea54327abf1ba99996cce424cc8ea20f114
DukhDmytro <cowboybebop4991@gmail.com>
2020-02-25 14:24:50 +0200

    Pagination

--- chat_test_task/settings.py
+++ chat_test_task/settings.py
@@ -137,3 +137,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
+
