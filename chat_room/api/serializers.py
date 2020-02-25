2693678c98ebb61f19cfa9e825b4013cdb51920b
DukhDmytro <cowboybebop4991@gmail.com>
2020-02-25 13:49:20 +0200

    Message and email validation

--- chat_room/api/serializers.py
+++ chat_room/api/serializers.py
@@ -1,3 +1,4 @@
+import re
 from chat_room.models import Messages
 from rest_framework import serializers
 
@@ -9,3 +10,19 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Messages
         fields = 'message', 'author', 'email', 'create_date', 'update_date', 'id'
+
+    def validate_email(self, email):
+        """Email validator"""
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if re.search(regex, email):
+            return email
+        raise serializers.ValidationError('Invalid email')
+
+
+    def validate_message(self, message):
+        """Message validation"""
+        rege = '^.{1,100}$'
+        print(message)
+        if re.fullmatch(rege, message):
+            return message
+        raise serializers.ValidationError('Message must contain 1-100 chars')
--- chat_room/models.py
+++ chat_room/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Messages(models.Model):
-    message = models.CharField(max_length=100)
+    message = models.TextField()
     author = models.CharField(max_length=50)
     email = models.EmailField()
     create_date = models.DateTimeField(auto_now_add=True)
